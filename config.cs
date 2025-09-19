a9ed9a39adb25368348ce46e4fdbc8ae24d92a1a
Lyvid Dev <lyviddev@gmail.com>
2025-09-18 21:47:34 -0400

    Setting Up Files..

---
+++ config.cs
@@ -0,0 +1,8 @@
+﻿namespace SpectatorHideRoles
+{
+    public class config
+    {
+        public bool HideSCPSpectate { get; set; } = true;
+        public bool HideTutorialSpectate { get; set; } = false;
+    }
+}
