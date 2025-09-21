144715a9ea70b74c0c5a5d1d36987f4b346fa8de
Lyvid Dev <lyviddev@gmail.com>
2025-09-21 01:01:20 -0400

    Delete SpectatorHideRoles directory

--- SpectatorHideRoles/LabAPI/config.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace SpectatorHideRoles
-{
-    public class config
-    {
-        public bool HideSCPSpectate { get; set; } = true;
-        public bool HideTutorialSpectate { get; set; } = false;
-    }
-}
