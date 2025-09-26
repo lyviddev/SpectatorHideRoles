e68b06c3b92acf898ed8b5d767b93646807bf03a
lyviddev <lyviddev@gmail.com>
2025-09-25 23:23:20 -0400

    wslkfherukihgybre

--- SpectatorHideRoles/Exiled/Plugin.cs
+++ SpectatorHideRoles/Exiled/Plugin.cs
@@ -9,7 +9,7 @@ using player = Exiled.Events.Handlers.Player;
 using customRoles = Exiled.CustomRoles.Events;
 
 public class Plugin : Plugin<Config> {
-    public override string Name => "SpectatorHideRoles";
+    public override string Name => "SpectatorHideRolesExiled";
     public override string Author => "Lyvid_Dev";
     public override Version RequiredExiledVersion => new(9, 9, 2);
     public override Version Version => new(1, 1, 1);
